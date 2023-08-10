2d5bc5482353bf180de4bf7063e2fb605d11295b
Guillermo Viveros <guillermoviveros@MacBook-Air-de-Guillermo.local>
2023-08-10 13:18:25 -0400

    Crear funcion que arroje un resultado al azar
    Crear funcion dentro de archivo js para la seleccion al azar de la computadora

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+/** La computadora arroja un resultado al azar
+ * Ya sea piedra,papel o tijeras **/
 function getComputerChoice(){
-  
+  choices = ["Piedra", "Papel", "Tijeras"]
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
 }
+
+/** El usuario debe ingresar su opcion en
+ * una ventana **/
+
+/** El juego debe tener 5 rondas **/
+
+/** Evaluar quien es el ganador **/
