60659d500df02dae61cc9309b2131968f9345f37
Guillermo Viveros <guillermoviveros@MacBook-Air-de-Guillermo.local>
2023-08-11 13:17:03 -0400

    Agregar funcion que permita jugar por 5 rondas
    Crear funcion que repita el juego por 5 rondas y diga el ganador

--- script.js
+++ script.js
@@ -14,37 +14,61 @@ function getPlayerChoice(){
   return playerChoice;
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
 function singleRound(playerSelection,computerSelection){
-  const ganador = "";
+  let ganador = "";
   if (playerSelection == "piedra" && computerSelection == "tijeras"){
     ganador = "jugador";
-    return alert("El jugador ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else if (playerSelection == "papel" && computerSelection == "piedra"){
     ganador = "jugador";
-    return alert("El jugador ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else if (playerSelection == "tijeras" && computerSelection == "papel"){
     ganador = "jugador";
-    return alert("El jugador ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else if (playerSelection == "piedra" && computerSelection == "papel"){
     ganador = "computadora";
-    return alert("La computadora ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else if (playerSelection == "papel" && computerSelection == "tijeras"){
     ganador = "computadora";
-    return alert("La computadora ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else if (playerSelection == "tijeras" && computerSelection == "piedra"){
     ganador = "computadora";
-    return alert("La computadora ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    return ganador;
   } else{
     return alert("Esta ronda ha sido un empate! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection );
   }
 }
 
-singleRound(playerSelection, computerSelection);
 /** El juego debe tener 5 rondas **/
 
+function game(){
+  let jugador = 0;
+  let computadora = 0;
+  let x = 0;
+  while (x < 5){
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    let ganador = singleRound(playerSelection,computerSelection);
+    if (ganador == "jugador"){
+      jugador += 1;
+      x += 1; 
+      alert("El jugador ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    }
+    else if (ganador == "computadora"){
+      computadora += 1;
+      x += 1; 
+      alert("La computadora ha ganado esta ronda! tu seleccionaste: " + playerSelection + " y la computadora: " + computerSelection);
+    }
+    else{
+      x += 1; 
+      jugador += 1;
+      computadora += 1;
+    }
+  }
+  alert("Juego terminado! obtuviste: " + jugador + " puntos, y la computadora: " + computadora + " puntos" )
+}
+
+game();
 
 
 /** Evaluar quien es el ganador **/
